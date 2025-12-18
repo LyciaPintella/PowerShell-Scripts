ecdf3d6d9831fbc38eead96263617adc5a16e192
LyciaPintella <jessica.ariadne.murphy@outlook.com>
2025-12-18 12:52:27 -0500

    Hi

--- Windows Troubleshooting/WebView2 Troubleshooting/Repair WebView2 For Widgets.ps1
+++ Windows Troubleshooting/WebView2 Troubleshooting/Repair WebView2 For Widgets.ps1
@@ -1,12 +1,16 @@
-#I ran the following code on my new user account:
+Get-AppxPackage MicrosoftWindows.Client.WebExperience |
+  ForEach-Object { Add-AppxPackage -DisableDevelopmentMode -Register "$($_.InstallLocation)\AppXManifest.xml" }
+
+# close Widgets / sign out first, then delete cache:
+Remove-Item -Path "$env:LOCALAPPDATA\Packages\MicrosoftWindows.Client.WebExperience_* \LocalCache" -Recurse -Force -ErrorAction SilentlyContinue
+
+Set-ItemProperty -Path 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced' -Name TaskbarDa -Value 1 -Type DWord
+
 
 Get-AppxPackage -AllUsers MicrosoftWindows.Client.WebExperience |
 ForEach-Object {
-	Remove-AppxPackage -AllUsers -Package $_.PackageFullName
+  Remove-AppxPackage -AllUsers -Package $_.PackageFullName
 }
-winget install Microsoft.WindowsWebExperiencePack --source msstore
 
-
-<#I needed to go to https://apps.microsoft.com/detail/9mssgkg348sp?hl=en-US&gl=US because winget couldn't find the experience host.
-
-Removing and reinstalling the web experience did case the widget panel to vanish for a moment on the new account, but it came back after the reinstall finished. However, my main account still cannot see the widget panel.#>
+cd "E:\OD\Jessica\OneDrive\Jess Files\Windows Application Installers"
+.\"Microsoft EdgeWebView2 Runtime x64.exe"
